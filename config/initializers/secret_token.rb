1a914e5374fc2d9e6469bef987d679dbb84c61b3
Terry <terry_lee_welch@hotmail.com>
2011-06-30 17:44:04 +0000

    Update code

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -4,4 +4,4 @@
 # If you change this key, all old signed cookies will become invalid!
 # Make sure the secret is at least 30 characters and all random,
 # no regular words or you'll be exposed to dictionary attacks.
-SampleApp::Application.config.secret_token = '47e2b7217e1677847bc41382ee63987e7fbebe63fbf5622b72ab5524d24a74339616ae75f278fbda914f7fc5957067151f020f5314cab534a5c8ba704bad29a6'
+Chatter::Application.config.secret_token = '47e2b7217e1677847bc41382ee63987e7fbebe63fbf5622b72ab5524d24a74339616ae75f278fbda914f7fc5957067151f020f5314cab534a5c8ba704bad29a6'
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,4 @@
-SampleApp::Application.routes.draw do
+Chatter::Application.routes.draw do
   
   resources :users
   resources :sessions, :only => [:new, :create, :destroy]
