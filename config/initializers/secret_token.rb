b51eb488f4c52ac4beb56734836c0f7f8f2b9107
Terry <terry_lee_welch@hotmail.com>
2011-06-30 17:09:05 +0000

    First Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Chatter::Application.config.secret_token = '4a43f9e659e92871516bddc105beb3a0d5a3fff8a85edf3211090c575676dd64b18594b91c4ead50292114ab7871b378d0b7807b22a5e188ae2f4fdfe851ccfb'
