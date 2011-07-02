1994e8b134c22f90ab8775803353b5fdc0b2453d
Terry <terry_lee_welch@hotmail.com>
2011-07-02 13:28:21 +0000

    destroy action added

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,6 +2,7 @@
   
   before_filter :authenticate, :only => [:index, :edit, :update]
   before_filter :correct_user, :only => [:edit, :update]
+  before_filter :admin_user,   :only => :destroy
   
   def new
     @user = User.new
@@ -45,6 +46,12 @@
     render 'edit'
     end
   end
+  
+  def destroy
+    User.find(params[:id]).destroy
+    flash[:success] = "User destroyed"
+    redirect_to users_path
+  end
 
 private 
 
@@ -57,5 +64,8 @@ private
     redirect_to(root_path) unless current_user?(@user)
   end
   
+  def admin_user
+    redirect_to(root_path) unless current_user.admin?
+  end
     
 end
